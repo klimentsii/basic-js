24288f602321c3380db76181afdd46ff094c2662
Klimentsii <59070287+klimentsii@users.noreply.github.com>
2021-09-16 13:04:32 +0300

    fix: new sol

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,21 +1,20 @@
 import { NotImplementedError } from '../extensions/index.js';
 
 export default function transform(arr) {
-  if (!(arr instanceof Array)) {
-    throw new Error(`'arr' parameter must be an instance of the Array!`);
-  }
-  
   let newarr = [];
+
+  if (!(arr instanceof Array)) {
+      throw new Error(`'arr' parameter must be an instance of the Array!`);
+  }
+
   for(let i = 0; i < arr.length; i++) {
     if(typeof(arr[i]) == "string") {
       if(arr[i] == "--discard-next")
-        newarr[i] = "", newarr[i + 1] = "", i++;
+        newarr[i] = "", newarr[i + 1] = "", arr[i+1] = "";
       if(arr[i] == "--discard-prev") 
         newarr[i] = "", newarr[i - 1]  = "";
       if(arr[i] == "--double-next") {
-        newarr[i] = "", newarr[i+1] = arr[i+1];
-        for(let j = i + 1; j < arr.length; j++) 
-          newarr[j+1] = arr[j], i++;
+        newarr[i] = arr[i+1], newarr[i+1] = arr[i+1];
       }
       if(arr[i] == "--double-prev") {
         newarr[i] = arr[i-1];
@@ -25,6 +24,6 @@ export default function transform(arr) {
         newarr[i] = arr[i];
       }
     }
-  }
+  } 
   return (newarr.filter(n => typeof(n) == "number"));
 }
