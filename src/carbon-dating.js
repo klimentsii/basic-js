bd8c43645ac0766031f48f85b46cfa5d4b2b6943
Klimentsii <59070287+klimentsii@users.noreply.github.com>
2021-09-16 15:00:11 +0300

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,7 +6,7 @@ const HALF_LIFE_PERIOD = 5730;
 export default function dateSample(sampleActivity) {
   let number = parseFloat(sampleActivity);
   const k = 0.693 / HALF_LIFE_PERIOD;
-  if(typeof sampleActivity != 'string' || (number <= 0 || number > 15) ){
+  if(Number.isNaN(number) || typeof sampleActivity != 'string' || (number <= 0 || number > 15) ){
     return false
   } else
     return Math.ceil((Math.log(MODERN_ACTIVITY / number)) / k);
