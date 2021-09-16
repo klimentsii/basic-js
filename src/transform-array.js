3c5716eee7583b33ed5c3c1f2daed4b6f7c22da2
Klimentsii <59070287+klimentsii@users.noreply.github.com>
2021-09-16 13:44:11 +0300

    Update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,38 +1,28 @@
 import { NotImplementedError } from '../extensions/index.js';
 
 export default function transform(arr) {
-  if(Array.isArray(arr) == true) {
-    if (!(arr instanceof Array)) {
-      throw new Error(`'arr' parameter must be an instance of the Array!`);
-    }
-    if(arr[0] == "--discard-prev" || arr[0] == "--double-prev" || arr[arr.length - 1] == "--discard-next" || arr[arr.length - 1] == "--double-next") {
-      throw new Error(`'arr' parameter must be an instance of the Array!`);
-    }
-
-    let newarr = [];
-
-    for(let i = 0; i < arr.length; i++) {
-      if(typeof(arr[i]) == "string" ) {
-        if(arr[i] == "--discard-next")
-          newarr[i] = "", newarr[i + 1] = "", arr[i+1] = "";
-        if(arr[i] == "--discard-prev") 
-          newarr[i] = "", newarr[i - 1]  = "";
-        if(arr[i] == "--double-next") {
-          newarr[i] = arr[i+1], newarr[i+1] = arr[i+1];
-        }
-        if(arr[i] == "--double-prev") {
-          newarr[i] = arr[i-1];
-        }
-      } else {
-        if(typeof(arr[i]) == "number") {
-          newarr[i] = arr[i];
-        }
-      }
-    } 
-    
-  return (newarr.filter(n => typeof(n) == "number"));
-  } else {
+  if (!(arr instanceof Array)) {
     throw new Error(`'arr' parameter must be an instance of the Array!`);
   }
+  let newarr = [];
+
+  if (Array.isArray(arr)) {
+      for(let i = 0; i < arr.length; i++) {
+          if(arr[i] === '--discard-next'){
+              i++;
+          } else if(arr[i] === '--discard-prev'){
+              if (newarr.length !== 0 && arr[i - 2] !== '--discard-next'){
+                  newarr.pop();
+              }
+          } else if(arr[i] === '--double-next'){
+              newarr.push(arr[i + 1]);
+          } else if(arr[i] === '--double-prev'){
+              if (i !== 0 && arr[i - 2] !== '--discard-next') {
+                  newarr.push(arr[i - 1]);
+              }
+          } else {
+              newarr.push(arr[i]);
+          }
+      };
 }
 
