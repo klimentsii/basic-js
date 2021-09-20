3cbf8e61858fa06289533931bf90fbcc067cede3
Klimentsii <59070287+klimentsii@users.noreply.github.com>
2021-09-21 00:16:39 +0300

    init: start chains

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,28 +1,31 @@
 import { NotImplementedError } from '../extensions/index.js';
 
-/**
- * Implement chainMaker object according to task description
- * 
- */
-export default {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+export default let chainMaker = {
+            str: '',
+
+            getLength() {
+                return this.str.split('.').length;
+            },
+            addLink(value) {
+                if(this.str === '')
+                    this.str += value;
+                else
+                    this.str += '.' + value;
+                return this;
+            },
+            removeLink(position) {
+                if(this.str != null || this.str != '') {
+                    this.str = this.str.split('.');
+                    this.str.splice(position, 1);
+                    this.str = this.str.join('.');
+                }
+                return this;
+            },
+            reverseChain() {
+                this.str = this.str.split(".").reverse().join(".");
+                return this;
+            },
+            finishChain() {
+                return '(' + this.str.split('.').join(')~~(') + ')';
+            }
 };
